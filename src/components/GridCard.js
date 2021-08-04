54144391bc080d086e8d9b8985859eeed573e2da
Gerald Gonzalez <gedgonz7@gmail.com>
2021-08-03 23:22:32 -0600

    initial app

---
+++ src/components/GridCard.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+useEffect(()=>{
+    
+})
+
+function GridCard() {
+    return (
+        <div className="row">
+            <div className="col-md-3"></div>
+        </div>
+    )
+}
+
+export default GridCard
