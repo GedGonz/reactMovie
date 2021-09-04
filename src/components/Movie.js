687157f13a3b388a8b16a28a21a3c8bc022cf57d
Gerald Gonzalez <gedgonz7@gmail.com>
2021-09-03 19:45:16 -0600

    component for view detail movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{ useEffect, useState } from "react"
+import { useParams } from "react-router";
+import { get } from "../services/movieApi";
+
 
 function Movie() {
+    
+    let { id } = useParams();
+
+    const [movi, setmovi] = useState(0);
+
+    useEffect(()=>{
+        finMovie(id);
+
+    },[id]);
+
+    const finMovie = (id)=>{
+
+        get(`movie/${id}`).then(resp=>{
+            setmovi(resp.data);
+        })
+
+    }
+
+    const imageUrl = "https://image.tmdb.org/t/p/w500" + movi.poster_path;
     return (
-        <div>
-            HOLA
+        
+        <div className="text-center">
+            <img alt="movies" className="cardMovie" src={imageUrl} />
+            <p className="text-center fw-bold">{movi.title}</p> 
         </div>
     )
 }
