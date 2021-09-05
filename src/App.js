b0f52fc761942cc79430b79667683fd8c051f0c1
Gerald Gonzalez <gedgonz7@gmail.com>
2021-09-05 16:25:00 -0600

    configure router for githubPages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Spinner from "./components/Spinner";
 function App() {
   return (
     
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Nav/>
         <div className="container-fluid">
           <Switch>
