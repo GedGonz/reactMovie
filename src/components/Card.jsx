e66f7ad0e09ddf920623c8ecdf326aed1caee812
Gerald Gonzalez <gedgonz7@gmail.com>
2021-09-05 16:53:26 -0600

    validating ruta with image not found

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router';
 import Avatar from '../components/Avatar';
 import "./Card.css";
 
 function Card(props) {
 
-    
+    let { id } = useParams();
+    let path = id? '../not_found.jpg':
+                   'not_found.jpg';
+
     const imageUrl =(props.movie.poster_path)? `https://image.tmdb.org/t/p/w${props.classname==="cardMovies"?"200":"500"}${props.movie.poster_path}`:
-                                                'not_found.jpg'; 
+                                                path; 
     
     return (
             <Avatar imageUrl={imageUrl} classname={props.classname}/>
